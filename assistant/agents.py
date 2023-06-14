2907963ff5c12391ba1112cd49d4c5f2780c0de5
ryan <778315181@qq.com>
2023-06-14 14:03:39 +0800

    1

--- assistant/agents.py
+++ assistant/agents.py
@@ -5,7 +5,7 @@ from langchain import OpenAI, SerpAPIWrapper, LLMChain
 from typing import List, Union
 from langchain.schema import AgentAction, AgentFinish
 import re
-template = """ chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
+template = """Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
 {tools}
 Use the following format:
 Question: the input question you must answer
@@ -18,73 +18,12 @@ Thought: I now know the final answer
 Final Answer: the final answer to the original input question
 Begin! Remember to speak as a pirate when giving your final answer. Use lots of "Arg"s
 Question: {input}
-{agent_scratchpad} 
- ASSISTANT:"""
-# Set up a prompt template
-class CustomPromptTemplate(StringPromptTemplate):
-    # The template to use
-    template: str
-    # The list of tools available
-    tools: List[Tool]
-    
-    def format(self, **kwargs) -> str:
-        # Get the intermediate steps (AgentAction, Observation tuples)
-        # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            # thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
-        kwargs["agent_scratchpad"] = thoughts
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
-        # Create a list of tool names for the tools provided
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        return self.template.format(**kwargs)
-    
-class CustomOutputParser(AgentOutputParser):
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, toolinput=action_input.strip(" ").strip('"'), log=llm_output)
-
-if __name__ == '__main__':
-    # 加载环境变量
-    from dotenv import load_dotenv,find_dotenv
-    load_dotenv(find_dotenv())
-    # 任务prompt
-    search = SerpAPIWrapper()
-    tools = [
-        Tool(
-            name = "Search",
-            func=search.run,
-            description="useful for when you need to answer questions about current events"
-        )
-    ]
-    prompt = CustomPromptTemplate(
-        template=template,
-        tools=tools,
-        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-        # This includes the `intermediate_steps` variable because that is needed
-        input_variables=["input", "intermediate_steps"]
-    )
-    
+{agent_scratchpad}"""
+# set refine template
+def vicuna_template_adjust(template):
+    '''利用fastchat api提供的vicuna模板来对模板进行微调
+    '''
     conv = get_conv_template("vicuna_v1.1")
-    conv.append_message(conv.roles[0], prompt.format(input='who you are?',intermediate_steps=[('1','setp1'),('2','step2')]))
+    conv.append_message(conv.roles[0], template)
     conv.append_message(conv.roles[1], None)
-    print(conv.get_prompt())
+    return  conv.get_prompt()
